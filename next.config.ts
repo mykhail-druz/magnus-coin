65608664bcd01a0584cc7ddd274d7be21d698aa7
Михаил <115717017+mykhail-druz@users.noreply.github.com>
2025-01-09 16:58:34 +0200

    Removed turbopack due to bugs and unpredicted behavior

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,29 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: false,
+            svgo: true,
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'removeViewBox',
+                  active: false,
+                },
+              ],
+            },
+          },
         },
-      },
-    },
+      ],
+    });
+
+    return config;
   },
 };
 
